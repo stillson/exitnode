8b31e81cd55d32fc1bdaf94bf6be7073b4638fb4
Chris Stillson <stillson@gmail.com>
2015-03-05 00:59:56 -0500

    more restructuring

--- src/etc/squid3/dnscheck.py
+++ src/etc/squid3/dnscheck.py
@@ -9,6 +9,7 @@ import stat
 from   tempfile import mkstemp, NamedTemporaryFile
 from   shutil import move
 import iptables_config
+from   itertools import product
 
 class Host:
     def __init__(self, name, url):
@@ -24,6 +25,12 @@ class Host:
         self.ip = socket.gethostbyname(self.url)
         return self
 
+    def dnsString(self):
+        return 'address=/' + self.url + '/' + self.ip
+
+    def searchString(self):
+        return 'address=/' + self.url
+
 hosts = [Host("apple", "apple.com"),
          Host("apple2", "captive.apple.com"),
          Host("apple3", "www.ibook.info"),
@@ -33,40 +40,60 @@ hosts = [Host("apple", "apple.com"),
          Host("apple7", "www.appleiphonecell.com"),
          Host("google", "clients3.google.com")]
 
+class DNSmasqconf(object):
+    file_name = '/etc/dnsmasq.conf'
+    proc_name = '/etc/init.d/dnsmasq'
+    def __init__(self):
+        self.object.__init__()
+        self.lines = False
 
-# From http://stackoverflow.com/questions/39086/search-and-replace-a-line-in-a-file-in-python
-# and modified to replace whole line
-def replace(file_path, pattern, subst):
-    with NamedTemporaryFile(delete=False) as new_file, open(file_path) as old_file:
-        for line in old_file:
-            if line and line[0] in '#\n':
-                new_file.write(line)
-                continue
-            if not pattern in line:
-                new_file.write(line)
-            else:
-                new_file.write(subst + '\n')
-    move(new_file.name, file_path)
+    def read(self):
+        "open file and read lines"
+        with open(self.file_name) as f:
+            self.lines = f.readlines()
+        return self
+
+    def replace(self, hosts):
+        "replace relevant lines"
+        def h_filt(line, host):
+            if host.searchString in line:
+                return host.dnsString
+            return line
+
+        # o(n^2). nothing's sorted. so sue me.
+        for i in product(self.lines, hosts):
+            hfilt(*i)
+
+
+    def close(self):
+        "close and rename"
+        with open(self.file_name + '.tmp', 'w') as f:
+            f.writeline(self.lines)
+        rename(self.file_name + '.tmp', self.file_name)
+        chmod(self.file_name, 0644)
+        iptables_config.add_rules()
+        subprocess.call([self.proc_name, 'restart'])
+
+    def run(self, hosts):
+        self.read()
+        self.replace(hosts)
+        self.close()
+
 
 
 def main():
-
     hosts = [i.getIp() for i in hosts]
     ip_list = [line.strip() for line in open('/etc/squid3/hosts')]
     needs_updating  = any([not i.inList(ip_list) for i in hosts])
 
     if needs_updating or FORCE:
+        D = DNSmasqconf()
         rename('/etc/squid3/hosts', '/etc/squid3/hosts.old')
         with open('/etc/squid3/hosts', 'w') as f:
             for host in hosts:
                 f.write(host.ip + '\n')
-                dns_string = 'address=/' + host.url + '/' + host.ip
-                replace('/etc/dnsmasq.conf', 'address=/' + host.url, dns_string) 
 
-        chmod('/etc/dnsmasq.conf', 0644)
-        iptables_config.add_rules()
-        subprocess.call(['/etc/init.d/dnsmasq', 'restart'])
-
+        D.run(hosts)
 
 
 if __name__ == "__main__":
